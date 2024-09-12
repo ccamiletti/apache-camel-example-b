a762800ab150f7ffaf37e221c4381393c5c1453c
Carlo Camiletti <carlo.camiletti.saez@ing.com>
2024-09-12 22:55:29 +0200

    first commit

---
+++ src/main/java/nl/ccs/camel/apache_camel_example_b/ApacheCamelExampleBApplication.java
@@ -0,0 +1,13 @@
+package nl.ccs.camel.apache_camel_example_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheCamelExampleBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheCamelExampleBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/ccs/camel/apache_camel_example_b/ApacheCamelExampleBApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.ccs.camel.apache_camel_example_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheCamelExampleBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
